ef4e3839f48d87bba19bdfd3a38b631282e7691d
Lauren Arfaras <larfaras96@gmail.com>
2017-09-05 22:37:46 -0400

    the listings are populating the table and the add a listing section is funcitoning

--- listingController.js
+++ listingController.js
@@ -1,13 +1,28 @@
-angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
+angular.module('listings').controller('ListingsController', ['$scope', 'Listings',
   function($scope, Listings) {
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
 
-    /* 
-      Implement these functions in the controller to make your application function 
-      as described in the assignment spec. 
+    /*
+      Implement these functions in the controller to make your application function
+      as described in the assignment spec.
      */
-    $scope.addListing = function() {};
+    $scope.addListing = function() {
+      $scope.listings.push({
+        "code": $scope.code,
+        "name": $scope.name,
+        "coordinates": {
+          "latitude": $scope.latitude,
+          "longitude": $scope.longitude
+        },
+        'address': $scope.address
+      });
+      $scope.code = "";
+      $scope.name = "";
+      $scope.latitude = "";
+      $scope.longitude = "";
+      $scope.address = "";
+    };
     $scope.deleteListing = function(index) {};
     $scope.showDetails = function(index) {};
   }

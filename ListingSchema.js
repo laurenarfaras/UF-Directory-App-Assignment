ba90c825318a28fa3219a1726b3258e9577ffa64
Lauren Arfaras <larfaras96@gmail.com>
2017-09-06 12:01:23 -0400

    created schema and attempted to connect to db

--- ListingSchema.js
+++ ListingSchema.js
@@ -1,10 +1,23 @@
 /* Import mongoose and define any variables needed to create the schema */
-var mongoose = require('mongoose'), 
+var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 /* Create your schema */
 var listingSchema = new Schema({
   /* your code here */
+  code: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  coordinates: {
+    longitude: Number,
+    latitude: Number
+  },
+  address: String
 });
 
 /* create a 'pre' function that adds the updated_at (and created_at if not already there) property */
